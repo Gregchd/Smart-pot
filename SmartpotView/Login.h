a533859f60edf7918119c2e46695eb188b03693a
Aldairmore <a20175844@pucp.edu.pe>
2023-10-04 17:25:19 -0500

    fix: ¨arreglando errores"

--- SmartpotView/Login.h
+++ SmartpotView/Login.h
@@ -73,9 +73,6 @@ namespace SmartpotView {
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->label5 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
-			// 
-			// label1
-			// 
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25));
 			this->label1->ForeColor = System::Drawing::Color::Green;
@@ -85,9 +82,6 @@ namespace SmartpotView {
 			this->label1->Size = System::Drawing::Size(158, 39);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Smat-Pot";
-			// 
-			// label2
-			// 
 			this->label2->AutoSize = true;
 			this->label2->Location = System::Drawing::Point(22, 106);
 			this->label2->Name = L"label2";
@@ -95,36 +89,24 @@ namespace SmartpotView {
 			this->label2->TabIndex = 1;
 			this->label2->Text = L"Usuario:";
 			this->label2->Click += gcnew System::EventHandler(this, &Login::label2_Click);
-			// 
-			// label3
-			// 
 			this->label3->AutoSize = true;
 			this->label3->Location = System::Drawing::Point(22, 158);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(120, 25);
 			this->label3->TabIndex = 2;
 			this->label3->Text = L"Contraseña:";
-			// 
-			// txtUsername
-			// 
 			this->txtUsername->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
 			this->txtUsername->Location = System::Drawing::Point(144, 101);
 			this->txtUsername->Name = L"txtUsername";
 			this->txtUsername->RightToLeft = System::Windows::Forms::RightToLeft::No;
 			this->txtUsername->Size = System::Drawing::Size(237, 30);
 			this->txtUsername->TabIndex = 3;
-			// 
-			// txtPassword
-			// 
 			this->txtPassword->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
 			this->txtPassword->Location = System::Drawing::Point(144, 156);
 			this->txtPassword->Name = L"txtPassword";
 			this->txtPassword->Size = System::Drawing::Size(237, 30);
 			this->txtPassword->TabIndex = 4;
 			this->txtPassword->UseSystemPasswordChar = true;
-			// 
-			// btnLogin
-			// 
 			this->btnLogin->BackColor = System::Drawing::Color::Green;
 			this->btnLogin->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
 			this->btnLogin->ForeColor = System::Drawing::Color::White;
@@ -135,9 +117,6 @@ namespace SmartpotView {
 			this->btnLogin->Text = L"Login";
 			this->btnLogin->UseVisualStyleBackColor = false;
 			this->btnLogin->Click += gcnew System::EventHandler(this, &Login::button1_Click);
-			// 
-			// label4
-			// 
 			this->label4->AutoSize = true;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10));
 			this->label4->ForeColor = System::Drawing::Color::Green;
@@ -147,9 +126,6 @@ namespace SmartpotView {
 			this->label4->TabIndex = 7;
 			this->label4->Text = L"Registrate";
 			this->label4->Click += gcnew System::EventHandler(this, &Login::label4_Click);
-			// 
-			// label5
-			// 
 			this->label5->AutoSize = true;
 			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10));
 			this->label5->Location = System::Drawing::Point(124, 210);
@@ -157,9 +133,6 @@ namespace SmartpotView {
 			this->label5->Size = System::Drawing::Size(96, 17);
 			this->label5->TabIndex = 8;
 			this->label5->Text = L"¿Eres nuevo\?";
-			// 
-			// Login
-			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(12, 25);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::Window;
